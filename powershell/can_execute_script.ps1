110ffecaa0552a115b2c755a12c41f6251937a85
Capsule-Man <shyalone_555@yahoo.co.jp>
2018-11-28 21:19:46 +0900

    policy

--- powershell/can_execute_script.ps1
+++ powershell/can_execute_script.ps1
@@ -15,9 +15,9 @@ Set-ExecutionPolicy RemoteSigned -Scope Process
 
 # ポリシー
 #
-# Restricted. Does not load configuration files or run scripts. Restricted is the default execution policy.
-# AllSigned. Requires that all scripts and configuration files be signed by a trusted publisher, including scripts that you write on the local computer.
-# RemoteSigned. Requires that all scripts and configuration files downloaded from the Internet be signed by a trusted publisher.
-# Unrestricted. Loads all configuration files and runs all scripts. If you run an unsigned script that was downloaded from the Internet, you are prompted for permission before it runs.
-# Bypass. Nothing is blocked and there are no warnings or prompts.
-# Undefined. Removes the currently assigned execution policy from the current scope. This parameter will not remove an execution policy that is set in a Group Policy scope.
+# Restricted. 
+# AllSigned. 
+# RemoteSigned. 
+# Unrestricted. 
+# Bypass. 
+# Undefined.
