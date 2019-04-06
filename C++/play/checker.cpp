d0181c223e386549a7ce7ab6f5eb68e5b0d30883
Capsule-Man <shyalone_555@yahoo.co.jp>
2019-04-07 00:20:45 +0900

    Train C++

--- C++/play/checker.cpp
+++ C++/play/checker.cpp
@@ -3,12 +3,14 @@
 #include "checker.h"
 
 
-bool check_sum(std::vector<double> vec, double threshold){
+bool check_sum(std::vector<double> const & vec, double threshold){
     const double result = std::accumulate(vec.begin(), vec.end(), 0.0);
     return result > threshold;
 }
 
-bool check_avg(std::vector<double> vec, double threshold){
+bool check_avg(std::vector<double> const & vec, double threshold){
+    // これで結果が変わってしまう!!
+    // const double result = std::accumulate(vec.begin(), vec.end(), 0) / std::size(vec);
     const double result = std::accumulate(vec.begin(), vec.end(), 0.0) / std::size(vec);
     std::cout << result << std::endl;
     return result > threshold;
@@ -16,7 +18,8 @@ bool check_avg(std::vector<double> vec, double threshold){
 
 int main() {
     std::vector<double> v = {1, 2, 3, 4};
-    if (check_avg(v, 2)) {
+    auto const & r = v;
+    if (check_avg(r, 2)) {
         std::cout << "BUY!!" << std::endl;
     }
     else {
--- C++/play/checker.h
+++ C++/play/checker.h
@@ -2,5 +2,5 @@
 
 #include <vector>
 
-bool check_sum(std::vector<double> vec, double threshold);
-bool check_avg(std::vector<double> vec, double threshold);
+bool check_sum(std::vector<double> const & vec, double threshold);
+bool check_avg(std::vector<double> const & vec, double threshold);
